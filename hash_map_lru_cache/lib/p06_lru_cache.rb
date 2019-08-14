8ad2027d129cf1edad066cc082822ac19b99da0c
cfo8473 <cfong7411@gmail.com>
2019-08-14 18:32:35 -0500

    Adds p06_lru_cache

--- hash_map_lru_cache/lib/p06_lru_cache.rb
+++ hash_map_lru_cache/lib/p06_lru_cache.rb
@@ -1,5 +1,6 @@
 require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
+require "byebug"
 
 class LRUCache
   def initialize(max, prc)
@@ -10,10 +11,15 @@ class LRUCache
   end
 
   def count
-    @map.count
+    map.count
   end
 
   def get(key)
+    debugger
+    unless map[key]
+      calc!(key)
+    end
+    map[key].val
   end
 
   def to_s
@@ -21,15 +27,31 @@ class LRUCache
   end
 
   private
+  attr_accessor :store, :map
+  attr_reader :max, :prc
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    node = Node.new(key, prc.call(key))
+    update_node!(node)
+    map.set(key, node)
+    eject! if count > max
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    node.prev.next = node.next if node.prev
+    node.next.prev = node.prev if node.next
+    store.tail.prev.next = node
+    node.prev = store.tail.prev
+    store.tail.prev = node
+    node.next = store.tail
   end
 
   def eject!
+    node = store.first
+    map.delete(node.key)
+    node.prev.next = node.next
+    node.next.prev = node.prev
   end
 end
